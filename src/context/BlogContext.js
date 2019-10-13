d93c17eecb3c7833dcaf177d0e8a0f265563e223
SHANbic <supagoof924@hotmail.com>
2019-10-13 19:06:02 +0200

    [ FEATURE ] Wired up actions from context object to actual rendered component

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -5,14 +5,14 @@ const BlogContext = React.createContext();
 export const BlogProvider = ({ children }) => {
   const [blogPosts, setBlogPosts] = useState([]);
 
-  const addBlogPosts = () => {
+  const addBlogPost = () => {
     setBlogPosts([
       ...blogPosts,
       { title: `Blog Post #${blogPosts.length + 1}` }
     ]);
   };
   return (
-    <BlogContext.Provider value={{ data: blogPosts, addBlogPosts }}>
+    <BlogContext.Provider value={{ data: blogPosts, addBlogPost }}>
       {children}
     </BlogContext.Provider>
   );
