0388f8af187fdd186a5487916631711112e4fd60
SHANbic <supagoof924@hotmail.com>
2019-10-17 21:20:07 +0200

    [ FEATURE ] Added edit screen and passed the id of the post to edit

---
+++ src/screens/EditScreen.js
@@ -0,0 +1,51 @@
+import React, { useContext, useState } from 'react';
+import { View, Text, StyleSheet, TextInput, Button } from 'react-native';
+import { Context } from '../context/BlogContext';
+
+const EditScreen = ({ navigation }) => {
+  const { addBlogPost, state } = useContext(Context);
+  const id = navigation.getParam('id');
+  const post = state.find(post => post.id === id);
+
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
+
+  return (
+    <View>
+      <Text style={styles.label}>Blog title</Text>
+      <TextInput
+        style={styles.inputText}
+        value={title}
+        onChangeText={text => setTitle(text)}
+      />
+      <Text style={styles.label}>Content</Text>
+      <TextInput
+        style={styles.inputText}
+        value={content}
+        onChangeText={text => setContent(text)}
+      />
+      <Button
+        title="Submit your post"
+        onPress={() =>
+          addBlogPost(title, content, () => navigation.navigate('Index'))
+        }
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  inputText: {
+    borderColor: 'black',
+    borderWidth: 1,
+    margin: 10,
+    padding: 5,
+    fontSize: 20
+  },
+  label: {
+    marginLeft: 5,
+    fontSize: 30
+  }
+});
+
+export default EditScreen;
