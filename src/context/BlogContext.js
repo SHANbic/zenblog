a3a5c9ffb5bae8ede1083e73175d717d50443c73
SHANbic <supagoof924@hotmail.com>
2019-10-17 20:46:13 +0200

    [ UPDATE ] Updated logic for submitting a post

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,17 +1,18 @@
-import { useReducer } from "react";
-import createDataContext from "./createDataContext";
+import { useReducer } from 'react';
+import createDataContext from './createDataContext';
 
 const blogReducer = (state, action) => {
   switch (action.type) {
-    case "add_blogpost":
+    case 'add_blogpost':
       return [
         ...state,
         {
           id: Math.floor(Math.random() * 999999),
-          title: `Blog Post #${state.length + 1}`
+          title: action.payload.title,
+          content: action.payload.content
         }
       ];
-    case "delete_blogpost":
+    case 'delete_blogpost':
       return state.filter(blogpost => blogpost.id !== action.payload);
     default:
       return state;
@@ -19,14 +20,20 @@ const blogReducer = (state, action) => {
 };
 
 const addBlogPost = dispatch => {
-  return () => {
-    dispatch({ type: "add_blogpost" });
+  return (title, content) => {
+    dispatch({
+      type: 'add_blogpost',
+      payload: {
+        title,
+        content
+      }
+    });
   };
 };
 
 const deleteBlogPost = dispatch => {
   return id => {
-    dispatch({ type: "delete_blogpost", payload: id });
+    dispatch({ type: 'delete_blogpost', payload: id });
   };
 };
 
